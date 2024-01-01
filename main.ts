63189bc33ccbb76aa42d2cc70c1ef3aadf9112e0
Thierry Crouzet <tc@tcrouzet.com>
2024-01-01 19:54:34 +0100

    DoubleEnter

--- main.ts
+++ main.ts
@@ -28,23 +28,27 @@ export default class FrenchTypos extends Plugin {
 			if (activeView) {
 				const editor = activeView.editor;
 				const cursor = editor.getCursor();
-				if (event.key === "'") {
+				if (event.key === "") {
 					event.preventDefault();
 					editor.replaceRange("’", cursor);
-					editor.setCursor({ line: cursor.line, ch: cursor.ch + 1 }); // Repositionne le curseur après l'apostrophe insérée
+					editor.setCursor({ line: cursor.line, ch: cursor.ch + 1 });
 				} else if (event.key === '"') {
 					event.preventDefault();
 					if (this.openQuote) {
-						editor.replaceRange("« ", cursor); // Insère un guillemet français ouvrant suivi d'un espace insécable
+						editor.replaceRange("« ", cursor);
 					} else {
-						editor.replaceRange(" »", cursor); // Insère un espace insécable suivi d'un guillemet français fermant
+						editor.replaceRange(" »", cursor);
 					}
-					editor.setCursor({ line: cursor.line, ch: cursor.ch + 2 }); // Repositionne le curseur après le guillemet inséré
-					this.openQuote = !this.openQuote; // Alterne l'état des guillemets
+					editor.setCursor({ line: cursor.line, ch: cursor.ch + 2 });
+					this.openQuote = !this.openQuote;
 				} else if (event.key === '-' && editor.getRange({ line: cursor.line, ch: cursor.ch - 1 }, cursor) === '-') {
 					event.preventDefault();
 					editor.replaceRange("— ", { line: cursor.line, ch: cursor.ch - 2 }, { line: cursor.line, ch: cursor.ch });
 					editor.setCursor({ line: cursor.line, ch: cursor.ch + 1 });
+				} else if (event.key === 'Enter') {
+					event.preventDefault();
+					editor.replaceRange("\n\n", cursor);
+					editor.setCursor({ line: cursor.line + 2, ch: cursor.ch });
 				}
 			}
 		});
